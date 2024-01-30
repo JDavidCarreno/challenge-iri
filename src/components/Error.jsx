1855ff50681925460d3044e68c4415baad871c46
juan carreno <carrenojuann97@gmail.com>
2024-01-30 12:41:10 -0500

    fix thankyou page

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,7 +1,13 @@
+import {useNavigate } from "react-router-dom"
+
 export function Error() {
+
+    const navigate = useNavigate()
+
     return (
         <>
             <h1> There was an error !!!</h1>
+            <button onClick={() => {navigate("/")}}>Go back</button>
         </>
     )
 }
--- src/components/Thankyou.jsx
+++ src/components/Thankyou.jsx
@@ -1,7 +1,13 @@
+import {useNavigate } from "react-router-dom"
+
 export function Thankyou() {
+    
+    const navigate = useNavigate()
+
     return(
         <>
             <h1>THANK YOU!!</h1>
+            <button onClick={() => {navigate("/")}}>Go back</button>
         </>
     )
 }
